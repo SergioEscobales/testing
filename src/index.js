685b97aeae204444b7175c06262f116c93d23903
Sergio Escobales <sescobalesortiz@gmail.com>
2020-06-17 12:47:12 +0200

    Comiting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import * as Sentry from '@sentry/browser';
+
+
+Sentry.init({dsn: "https://5bf051733d1742fd9095dc6da59e991e@o408693.ingest.sentry.io/5279832"});
+
 
 ReactDOM.render(<App/>, document.getElementById("root"));
